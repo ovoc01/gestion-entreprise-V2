4458ac809199d07a780c37534454f259c80d155f
ovoc01 <spotifymirindra@gmail.com>
2023-10-21 11:22:12 +0300

    first commit

---
+++ spring-server/src/test/java/com/entreprise/gestion/GestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entreprise.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
